452a4df41b20db6b621760c51c55c5136012b1be
priyank3487 <pikachu.sinchan.23450@gmail.com>
2021-12-23 19:15:17 +0530

    emmiting custome events componenets

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<section>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+ <!-- <HelloWorld msg="Welc ome to Your Vue.js App"/> -->
+  <h2>my friends</h2>
+  </section>
+<friend-contact
+v-for ="friend in friends"
+:key="friend.id"
+:id="friend.id"
+ :name="friend.name"
+ :phone-number="friend.phone"
+ :email-address="friend.address"
+ :is-favourite="friend.isFavourite"
+ @toggle-contact="toggleFavouriteStatus"
+></friend-contact> 
+<!-- <friend-contact
+ name="adelina gomez"
+ phone-number="64568522"
+ email-address="adelina gomez@gmail.com"
+ is-favourite = "0"
+></friend-contact>  -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+   data(){
+        return{
+           
+            friends : [
+              {
+                  id : 'manual',
+                  name : 'priyanka saha',
+                  phone: '89652347',
+                  email : 'sahae455@gmail.com',
+                  isFavourite: true
+              },
+               {
+                  id : 'manual',
+                  name : 'adelina gomez',
+                  phone: '64568522',
+                  email : 'adelina gomez@gmail.com',
+                  isFavourite: false
+              },
+               
+            ]
+
+        };
+    },
+    methods :{
+      toggleFavouriteStatus(friendId){
+       const identifiredFriend = this.friends.find( 
+         (friend) => friend.id === friendId)
+         ;
+       identifiredFriend.isFavourite = !identifiredFriend.isFavourite;
+      },
+    },
+
+};
 </script>
 
 <style>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,73 @@
+<template>
+<li>
+    <h2>{{ name }} {{ isFavourite ? '(Favourite)' : '' }}</h2>
+    <button @click="toggleFavourite">Toggle Favourite</button><br>
+    <button @click="toggledetails">{{ detailsAreVisible ? 'hide' : 'show' }} details</button>
+    <ul v-if="detailsAreVisible">
+        <li>
+            <strong>phone:</strong>
+            {{ phoneNumber }}
+            </li>
+        <li>
+            <strong>email:</strong>
+            {{ emailAddress }}
+        </li>
+        
+    </ul>
+</li>
+</template>
+<script>
+export default{
+    // props : ['name','phoneNumber','emailAddress','isFavourite' ],
+    props : {
+        id : {
+        type: String,   
+        required: true
+        },
+    name : {
+        type: String,
+        required: true
+    },
+    phoneNumber :{
+         type: String,
+        required : true  
+         },
+    emailAddress :{
+         type: String,
+        required : true  
+         },
+    isFavourite :{
+       type : Boolean,
+       required : false,
+       default : false  
+    } ,    
+    },
+      data(){
+          return{
+              detailsAreVisible : false
+            //    friend : 
+            //   {
+            //       id : 'manual',
+            //       name : 'priyanka saha',
+            //       phone: '89652347',
+            //       email : 'sahae455@gmail.com'
+            //   },
+            //   friendIsFavourite : this.isFavourite,
+          };
+      },
+      methods :{
+              toggledetails(){
+                  this.detailsAreVisible = !this.detailsAreVisible ;
+              },
+              toggleFavourite(){
+                //   if(this.friendIsFavourite === '1'){
+                //       this.friendIsFavourite = '0';
+                //       }else{
+                //           this.friendIsFavourite = '1';
+                //       }
+                // this.friendIsFavourite = !this.friendIsFavourite ;
+                this.$emit('toggle-contact', this.id);
+              }
+      }
+}
+</script>
